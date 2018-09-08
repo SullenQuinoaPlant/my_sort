b752f359efeebff01a3ecc8b61bf0d5bc97e2e2b
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-08 11:14:57 +0200

    i

--- sources/sort_ar_ip.c
+++ sources/sort_ar_ip.c
@@ -1,3 +1,27 @@
+static void					last_cmp(
+	unsigned char *buf,
+	t_sort_ard ars[],
+	t_sort_cmp cmp,
+	void *pivot)
+{
+	t_sort_ard	*const small = &ars[e_less];
+	void		*const p = small->ar + small->sz * small->elem_count; 
+
+	if ((*cmp)(p, pivot) < SORT_EQ)
+		small->elem_count++;
+	else
+		swap(p, pivot, buf, small->type_sz);
+}
+
+static void					sort_some_more(
+	unsigned char *buf,
+	t_sort_ord ars[],
+	t_sort_cmp cmp)
+{
+	sort(buf, &ars[e_more], cmp);
+	sort(buf, &ars[e_less], cmp);
+}
+
 /*
 ** swaps on less_than to shorten the comparison time (maybe)
 **  -> only one bit to check for >= on two's complement machine
@@ -13,22 +37,25 @@ static void					sort(
 	void			*stop;
 	void			*p;
 
+	sub_ars[e_less] = (t_s_sort_ard){sz, 0, ar->ar};
+	sum_ars[e_more] = (t_s_sort_ard){sz, ar->elem_count, pivot};
 	stop = ar->ar;
-	sub_ars[e_less] = (t_s_sort_ard){sz, 0, stop};
-	sum_ars[e_more] = (t_s_sort_ard){sz, 1, pivot};
 	p = pivot - sz;
-	while (p >= stop)
+	while (p > stop)
 		if ((*cmp)(p, pivot) < SORT_EQ)
 		{
 			swap(p, stop, buf, sz);
-			stop += sz;
 			sub_ars[e_less].elem_count++;
+			stop += sz;
 		}
 		else
-		{
 			p -= sz;
-			sub_ars[e_more].elem_count++;
-		}
+	if ((*cmp)(p, pivot) < SORT_EQ)
+		small->elem_count++;
+	else
+		swap(p, pivot, buf, small->type_sz);
+	sub_ars[e_more].elem_count -= sub_ars[e_less].elem_count;
+	sort_some_more(buf, sub_ars, cmp);
 }
 
 void						sort_ar_ip(
