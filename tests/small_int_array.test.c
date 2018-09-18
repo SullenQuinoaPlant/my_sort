08d7c4153a58e7c95038adaef64a2477c1aac5ce
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-18 05:59:58 +0200

    i

--- tests/small_int_array.test.c
+++ tests/small_int_array.test.c
@@ -8,11 +8,31 @@
 
 int	declare_tests_and_run(int all_of, char *these[])
 {
-	#define SZ 20
-	#define SZM1 SZ - 1
+	#define SZ 3 
+	int			*ref_ar;
+	ref_ar = ((int[SZ]){0, 1, 2});
+	#define SZM1 (sizeof(int) * (SZ - 1))
 
-	int			*ref_ar_20;
-	ref_ar_20 = ((int[SZ]){
+	T(tiny_ar,
+		int				ar[SZ];
+		size_t			i;
+		t_s_sort_ard	ard;
+
+		time_t			time_1;
+		time_t			time_2;
+
+		for (i = 0; i < SZ; i++)
+			ar[i] = i;
+		ard = ((t_s_sort_ard){sizeof(int), ar, ar + SZM1});
+		time(&time_1);
+		sort_ar_ip(&int_ord, &ard);
+		time(&time_2);
+		printf("sorted in %llu\n", (unsigned long long)(time_2 - time_1));
+		assert_memory_equal(ar, ref_ar, SZ * sizeof(int));
+	)
+	#undef SZ
+	#define SZ 20
+	ref_ar = ((int[SZ]){
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 			10, 11, 12, 13, 14, 15, 16, 17, 18, 19});
 
@@ -31,7 +51,7 @@ int	declare_tests_and_run(int all_of, char *these[])
 		sort_ar_ip(&int_ord, &ard);
 		time(&time_2);
 		printf("sorted in %llu\n", (unsigned long long)(time_2 - time_1));
-		assert_memory_equal(ar, ref_ar_20, SZ * sizeof(int));
+		assert_memory_equal(ar, ref_ar, SZ * sizeof(int));
 	)
 
 	T(rev_ar,
@@ -49,7 +69,7 @@ int	declare_tests_and_run(int all_of, char *these[])
 		sort_ar_ip(&int_ord, &ard);
 		time(&time_2);
 		printf("sorted in %llu\n", (unsigned long long)(time_2 - time_1));
-		assert_memory_equal(ar, ref_ar_20, SZ * sizeof(int));
+		assert_memory_equal(ar, ref_ar, SZ * sizeof(int));
 	)
 
 	return(run_test_arr(all_of, these));
